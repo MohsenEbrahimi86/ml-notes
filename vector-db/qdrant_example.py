e6bce977b814d752cfc7376e8326c8ad120bf831
Shogath <Shogath@live.com>
2025-09-05 21:04:14 +0330

    Add qdrant example

---
+++ vector-db/qdrant_example.py
@@ -0,0 +1,19 @@
+import os
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+
+QDRANT_URL = os.getenv('QDRANT_URL')
+QDRANT_COLLECTION = os.getenv('QDRANT_COLLECTION')
+
+client = QdrantClient(url=QDRANT_URL)
+
+
+if not client.collection_exists(QDRANT_COLLECTION):
+    response = client.create_collection(
+        collection_name=QDRANT_COLLECTION,
+        vectors_config=VectorParams(size=4, distance=Distance.DOT),
+    )
+
+    print(response)
+else:
+    print(f"The collection: {QDRANT_COLLECTION} exists.")
